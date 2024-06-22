0e00314993a9173163825dea272d5ae6e1df0e63
jahnavi0545 <kandiboyinajahnavi@gmail.com>
2024-06-22 11:28:34 +0530

    test

--- jaanufor.java
+++ jaanufor.java
@@ -2,25 +2,17 @@ package Batch2;
 
 import java.util.Scanner;
 
-public class jaanuwhile {
+public class jaanufor {
     public static void main(String[] args) {
-        
         Scanner sc=new Scanner(System.in);
         System.out.println("enter a starting number");
         int startingno=sc.nextInt();
         System.out.println("enter a ending number");
         int endingno=sc.nextInt();
-        //int number=startingno;
-        
-        while(startingno<=endingno)
-        {
-
-            if(startingno%2==0)
-            {
-                System.out.println("even numbers is: "+startingno);
-                
+        for(int number=startingno; number<=endingno; number++){
+            System.out.println(number);
             }
-            startingno++;
-    }
-    }
+        }
+        
 }
+
